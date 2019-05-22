299afa5cf4d65375286e3885cc965290f14c1d22
Viachaslau Pasedzka <Viachaslau.Pasedzka@itechart-group.com>
2019-05-22 16:22:08 +0300

    structure last version

--- proj/urls.py
+++ proj/urls.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework import routers
-from products.views import CategoryViewSet, BrandViewSet, ProductViewSet
-
-
-router = routers.DefaultRouter()
-router.register(r'products', ProductViewSet)
-router.register(r'brands', BrandViewSet)
-router.register(r'categories', CategoryViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
