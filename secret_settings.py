9b2994cdfdfa7a53bfc64a2244fdb99b6f5cb207
SPasedko <mefi-100@tut.by>
2019-05-23 20:30:45 +0300

    product models added

---
+++ secret_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'ancsye9__ty%en75oy)gz957axc+(nfl-3ao(bx7==*b4#y$z6'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER': 'postgres',
+        'PASSWORD': 'corn25',
+        'HOST':'localhost'
+
+    }
+}
