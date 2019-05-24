a3c2c615019bdba24ad47103c1079cd4dc28d4c2
Viachaslau Pasedzka <Viachaslau.Pasedzka@itechart-group.com>
2019-05-24 10:37:55 +0300

    selfies deleted

--- products/models.py
+++ products/models.py
@@ -2,43 +2,43 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True) 
+    name = models.CharField(max_length=50, unique=True)
 
-    def __str__(self): 
-        return self.name 
+    def __str__(self):
+        return self.name
 
 
-class Brand(models.Model): 
-    name = models.CharField(max_length=50, unique=True) 
-    description = models.TextField() 
-    image = models.ImageField(upload_to='photos/brands', verbose_name='Brand`s photo') 
+class Brand(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='photos/brands', verbose_name='Brand`s photo')
 
-    def __str__(self): 
-        return self.name 
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=60) 
-    description = models.TextField() 
-    price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2) 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE) 
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='photos/products', verbose_name='Product`s photo')
 
     def __str__(self):
         return self.name
 
 
-class Attribute(models.Model): 
+class Attribute(models.Model):
     name = models.CharField(max_length=60)
 
     def __str__(self):
         return self.name
 
 
-class ProductAttribute(models.Model): 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
-    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE) 
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
     value = models.CharField(max_length=100)
 
     def __str__(self):
