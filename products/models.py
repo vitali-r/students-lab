6012e73957295b690cc669b2f21d1e9f4c2cbe10
Viachaslau Pasedzka <Viachaslau.Pasedzka@itechart-group.com>
2019-05-24 11:13:47 +0300

    serializers v0.0.1

--- products/models.py
+++ products/models.py
@@ -22,8 +22,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='photos/products', verbose_name='Product`s photo')
+    brand = models.ForeignKey(Brand, related_name='products' on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/products', related_name='products', verbose_name='Product`s photo')
 
     def __str__(self):
         return self.name
@@ -38,7 +38,7 @@ class Attribute(models.Model):
 
 class ProductAttribute(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Attribute, related_name='product_attributes', on_delete=models.CASCADE)
     value = models.CharField(max_length=100)
 
     def __str__(self):
---
+++ products/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from products.models import Category, Brand, Product, Attribute, ProductAttribute
+
+
+class AttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attribute
+        fields = ('id', 'name')
+
+
+class ProductAttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductAttribute
+        fields = (
+            'id',
+            'product',
+            'attribute',
+            'value'
+        )
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    product_attributes = ProductAttributeSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'description',
+            'price',
+            'image',
+            'brand',
+            'category'
+            'product_attributes'
+        )
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Brand
+        fields = (
+            'id',
+            'name',
+            'description',
+            'image',
+            'products'
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'products')
