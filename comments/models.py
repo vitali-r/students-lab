f7e5dff8453a56d5912c99c24aa6c09576ed32b2
impenetrabl1 <qwerqweqwr214@gmail.com>
2019-05-27 18:36:51 +0300

    change fields names, add only one migrations file

--- comments/models.py
+++ comments/models.py
@@ -11,14 +11,20 @@ class Comment(models.Model):
     THREE = '3' 
     TWO = '2' 
     ONE = '1' 
-    MARK_CHOICES = ( (FIVE, '5'), (FOUR, '4'), (THREE, '3'), (TWO, '2'), (ONE, '1') ) 
+    MARK_CHOICES = ( 
+        (FIVE, '5'), 
+        (FOUR, '4'), 
+        (THREE, '3'), 
+        (TWO, '2'), 
+        (ONE, '1') 
+    ) 
     text = models.TextField(verbose_name='Text')
     mark = models.CharField(max_length=1, verbose_name='Mark', choices=MARK_CHOICES, default=FIVE)
-    user_id = models.ForeignKey(User, related_name='comment', on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='comment', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
-        return "Comment on product - {}, added by user - {}".format(self.product_id, self.user_id)
+        return "Comment on product - {}, added by user - {}".format(self.product, self.user)
 
     class Meta:
         verbose_name = "Comment"
