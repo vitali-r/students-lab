292511faf1bea06a7a556c009b02b733d30b3921
impenetrabl1 <qwerqweqwr214@gmail.com>
2019-05-27 16:59:18 +0300

    added constants for choices, fixed __str__ representation

--- comments/models.py
+++ comments/models.py
@@ -6,15 +6,20 @@ from products.models import Product
 # Create your models here.
 
 class Comment(models.Model):
-    MARK_CHOICES = ( ('5', '5'), ('4', '4'), ('3', '3'), ('2', '2'), ('1', '1') )
+    FIVE = '5' 
+    FOUR = '4' 
+    THREE = '3' 
+    TWO = '2' 
+    ONE = '1' 
+    MARK_CHOICES = ( (FIVE, '5'), (FOUR, '4'), (THREE, '3'), (TWO, '2'), (ONE, '1') ) 
     text = models.TextField(verbose_name='Text')
-    mark = models.CharField(max_length=1, verbose_name='Mark', choices=MARK_CHOICES)
+    mark = models.CharField(max_length=1, verbose_name='Mark', choices=MARK_CHOICES, default=FIVE)
     user_id = models.ForeignKey(User, related_name='comment', on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.text
-    
+        return "Comment on product - {}, added by user - {}".format(self.product_id, self.user_id)
+
     class Meta:
         verbose_name = "Comment"
         verbose_name_plural = "Comments"
