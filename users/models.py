4fb7a0eca7503255c76f486872029614f9927702
Zomba4ok <Antickz28@gmail.com>
2019-05-27 18:26:40 +0300

    splitted address atribute

--- users/models.py
+++ users/models.py
@@ -3,7 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    address = models.CharField(max_length=256, blank=True)
+    address_country = models.CharField(max_length=100, blank=True)
+    address_region = models.CharField(max_length=100, blank=True)
+    address_city = models.CharField(max_length=100, blank=True)
+    address_street = models.CharField(max_length=100, blank=True)
+    address_home_number = models.CharField(max_length=100, blank=True)
+    address_room_number = models.CharField(max_length=100, blank=True)
     phone = models.CharField(max_length=18)
     avatar = models.ImageField(upload_to='photos/profiles', verbose_name='User\'s photo', blank=True)
     zip_code = models.CharField(max_length=12, blank=True)
+
+    def get_adress(self):
+        adress = self.address_home_number + ' ' + self.address_street + '\n' + self.address_city + '\n' + self.address_region + ' ' + self.zip_code + '\n' + self.address_country
+        if not(self.address_room_number == ''):
+            adress = 'Apt. ' + self.address_room_number + ' ' + adress
+        return adress
