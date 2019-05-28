4c2dddcded7eb5d8a3766ef3321c787d35997e7d
impenetrabl1 <qwerqweqwr214@gmail.com>
2019-05-28 18:43:35 +0300

    PEP8 fix

--- comments/views.py
+++ comments/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render 
-from rest_framework import viewsets, mixins, generics 
-from comments.serializers import CommentsSerializer 
-from comments.models import Comment 
-from rest_framework.permissions import IsAuthenticatedOrReadOnly 
+from django.shortcuts import render
+from rest_framework import viewsets, mixins, generics
+from comments.serializers import CommentsSerializer
+from comments.models import Comment
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class CommentsViewSet(viewsets.ModelViewSet): 
-    queryset = Comment.objects.all() 
-    serializer_class = CommentsSerializer 
+class CommentsViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentsSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
