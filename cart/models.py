213116c1af146c1544fd1e37939bfae063be7411
asidonlylips <asidonlylips@sad.captain.github.com>
2019-05-30 13:47:11 +0300

    added cart models

---
+++ cart/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Cart of user - {}".format(self.user)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.product.name
--- proj/settings.py
+++ proj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'products',
     'users',
     'comments',
+    'cart',
 ]
 
 REST_FRAMEWORK = {
